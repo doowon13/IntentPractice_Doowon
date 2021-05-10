c8183d58c075e54d681ed6c98014b98347c99cb3
doowon13 <endnjs2007@gmail.com>
2021-05-10 18:59:13 +0900

    메세지를 보내는 화면과 받는화면 추가

---
+++ app/src/main/java/com/pluto/intentpractice_doowon/MessageActivity.kt
@@ -0,0 +1,11 @@
+package com.pluto.intentpractice_doowon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
