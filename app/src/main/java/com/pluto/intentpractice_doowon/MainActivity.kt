258915e1e7da6c4c50813eeceba88296e982ae74
doowon13 <endnjs2007@gmail.com>
2021-05-10 11:36:39 +0900

    Intent를 이용해서 다른화면으로 이동해 보았다.

--- app/src/main/java/com/pluto/intentpractice_doowon/MainActivity.kt
+++ app/src/main/java/com/pluto/intentpractice_doowon/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.pluto.intentpractice_doowon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        moveToOtherBtn.setOnClickListener {
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
