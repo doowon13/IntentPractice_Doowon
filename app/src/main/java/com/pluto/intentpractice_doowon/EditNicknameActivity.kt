20c289ec4421298f673303f8f46e16e8cd3b0e05
doowon13 <endnjs2007@gmail.com>
2021-05-11 18:38:13 +0900

    닉네임 변경을 위한 화면 이동 startActivityForResult 활용

--- app/src/main/java/com/pluto/intentpractice_doowon/EditNicknameActivity.kt
+++ app/src/main/java/com/pluto/intentpractice_doowon/EditNicknameActivity.kt
@@ -2,10 +2,14 @@ package com.pluto.intentpractice_doowon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+
+
     }
 }
